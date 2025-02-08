c4da7de2ea11ae99c56be4c16d94cb62ee1fe59a
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-08 12:58:02 +0600

    add github login

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,8 +1,28 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
   return (
-    <div>
+    <>
       <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
-    </div>
+      {session?.user ? (
+        <div className="flex justify-center items-center flex-col mt-10">
+          <Image
+            width={100}
+            height={100}
+            className="rounded-full"
+            src={session?.user?.image || "/default-avatar.png"}
+            alt="user image"
+          />
+          <h2 className="text-xl font-semibold">{session?.user?.name}</h2>
+          <h2 className="text-xl font-semibold">{session?.user?.email}</h2>
+        </div>
+      ) : (
+        <div>Not Data found</div>
+      )}
+    </>
   );
 };
 
